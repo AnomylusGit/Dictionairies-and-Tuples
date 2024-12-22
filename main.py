41dbdd321c65b0f39a8897d6955d7ba958cec28c
AnomylusGit <urath.aarush13@gmail.com>
2024-12-22 12:57:12 +0200

    Dict+tuples

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-my_dict = {"name": "Aarush", "age": 11}
+my_dict = {1:10, 2:20, 3:30, 4:40, 5:50, 6:60}
 
-print(my_dict["name"])
-print(my_dict.get("age"))
+print(my_dict[1:10])
+print(my_dict.get(2:20))
 
-my_dict["age"] = 12
+my_dict[3:30] = 12
 print(my_dict)
 
-my_dict["address"] = "North-East"
+my_dict[4:30] = "North-East"
 print(my_dict)
 
-my_dict.pop("age")
+my_dict.pop(3:30)
 print(my_dict)
 
 print("Address :", my_dict.get("address"))
